5c4bd90baf301f5ef35f40ee9fbf4afa8b2c8d28
ivan dosca <l22210786@tectijuana.edu.mx>
2025-12-10 21:08:25 -0800

    Add IEstadoEstudiante interface with MostrarEstado method

---
+++ SistemaEducativo/State/IEstadoEstudiante.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEducativo
+{
+    public interface IEstadoEstudiante
+    {
+        void MostrarEstado();
+    }
+}
